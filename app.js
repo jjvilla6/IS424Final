47d95b837da778df647db7e4b0d40357b6ed625c
jjvilla6 <55564846+jjvilla6@users.noreply.github.com>
2022-03-27 20:26:57 -0500

    Added Modals

---
+++ app.js
@@ -0,0 +1,67 @@
+// New Appointment Button
+var addAppointmentBtn = document.querySelector("#addAppointmentBtn");
+addAppointmentBtn.addEventListener('click', (e) => {
+    var appointmentForm = document.querySelector("#appointmentForm");
+    // show modal
+    addAppointmentModal.classList.add('is-active');
+    e.preventDefault();
+})
+var appointmentCancel = document.querySelector("#appointmentCancel");
+appointmentCancel.addEventListener('click', (e) => {
+    e.preventDefault();
+    addAppointmentModal.classList.remove('is-active');
+})
+var appointmentReset = document.querySelector("#appointmentReset");
+appointmentReset.addEventListener('click', (e) => {
+    e.preventDefault();
+    appointmentForm.reset();
+})
+var addAppointmentModalBG = document.querySelector("#addAppointmentModalBG")
+addAppointmentModalBG.addEventListener("click", (e) => {
+    e.preventDefault();
+    addAppointmentModal.classList.remove('is-active');
+})
+// New Contact Button
+var addContactBtn = document.querySelector("#addContactBtn");
+addContactBtn.addEventListener('click', (e) => {
+    var contactFrom = document.querySelector("#contactForm");
+    // show modal
+    addContactModal.classList.add('is-active');
+    e.preventDefault();
+})
+var contactCancel = document.querySelector("#contactCancel");
+contactCancel.addEventListener('click', (e) => {
+    e.preventDefault();
+    addContactModal.classList.remove('is-active');
+})
+var contactReset = document.querySelector("#contactReset");
+contactReset.addEventListener('click', (e) => {
+    e.preventDefault();
+    contactForm.reset();
+})
+var addContactModalBG = document.querySelector("#addContactModalBG")
+addContactModalBG.addEventListener("click", (e) => {
+    e.preventDefault();
+    addContactModal.classList.remove('is-active');
+})
+//Signupbtn Functionality
+let signupbtn = document.querySelector('#signupbtn');
+let signupModal = document.querySelector('#signup-modal');
+let signupModalBg = document.querySelector('#signupmodalBG');
+signupbtn.addEventListener('click', () => {
+    signupModal.classList.add('is-active');
+});
+signupModalBg.addEventListener('click', () => {
+    signupModal.classList.remove('is-active');
+});
+//Sign-inModal Functionality
+let signinbtn = document.querySelector('#signinbtn');
+let signinModal = document.querySelector('#signin-modal');
+let signinModalBg = document.querySelector('#signinmodalBG');
+signinbtn.addEventListener('click', () => {
+    signinModal.classList.add('is-active');
+})
+
+signinModalBg.addEventListener('click', () => {
+    signinModal.classList.remove('is-active');
+});
